4eb7ab72b6698c9f7c7dfeb2f48d65af395ba354
Charles Papandreou <cnp20@duke.edu>
2020-01-25 19:48:32 -0500

    Added method shells and created constructor

---
+++ src/cellsociety/Grid.java
@@ -0,0 +1,40 @@
+package cellsociety;
+
+import javafx.scene.layout.GridPane;
+
+public class Grid {
+    private Cell[][] gridStructure;
+    private GridPane gridVisual;
+    private String gridType;
+    private int size;
+
+    public Grid(String xmlFilename){
+        parseFile(xmlFilename);
+        initializeGridStructure();
+        initializeGridVisual();
+    }
+
+    public void step(){
+
+    }
+
+    private void parseFile(String xmlFilename) {
+
+    }
+
+    private void initializeGridStructure() {
+
+    }
+
+    private void initializeGridVisual() {
+
+    }
+
+    private void calcNewStates(){
+
+    }
+
+    private void calcNewStates(){
+
+    }
+}
