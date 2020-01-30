2dc910dc4d2c131157b82b821d8be1415e6aa12f
Charles Papandreou <cnp20@duke.edu>
2020-01-30 15:31:37 -0500

    Added required method shells

--- src/cellsociety/FireCell.java
+++ src/cellsociety/FireCell.java
@@ -1,4 +1,16 @@
 package cellsociety;
 
 public class FireCell extends Cell {
+
+    public FireCell(double width, double height, String currState){
+        super(width,height,currState);
+    }
+
+    public void calcNewState(){
+
+    }
+
+    public void changeDisplay(){
+
+    }
 }
