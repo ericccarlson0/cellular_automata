597a1090091ef432c98e9c0fa90a003f495a8181
Charles Papandreou <cnp20@duke.edu>
2020-02-01 22:53:26 -0500

    Can successfully check if simtype provided is valid

--- src/cellsociety/XMLParser.java
+++ src/cellsociety/XMLParser.java
@@ -5,6 +5,10 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
@@ -12,9 +16,41 @@ import org.xml.sax.SAXException;
 
 public class XMLParser {
     private final DocumentBuilder DOCUMENT_BUILDER;
+    private ArrayList<String> validSimTypes;
 
     public XMLParser(){
         DOCUMENT_BUILDER = getDocumentBuilder();
+        Grid.SimulationType[] validSims = Grid.SimulationType.values();
+        validSimTypes = new ArrayList<String>();
+        for(Grid.SimulationType sim : validSims){
+            validSimTypes.add(sim.name());
+        }
+        System.out.println(validSimTypes.get(0));
+    }
+
+    public Grid generateGrid(String xmlFilename){
+        System.out.println("*");
+        File dataFile = new File(xmlFilename);
+        System.out.println("**");
+        Element root = getRootElement(dataFile);
+        if (!isValidFile(root)) {
+            System.out.println("INVALID!!!");
+            return null;
+        }
+        else{
+            System.out.println("VALID!!!");
+            return null;
+        }
+    }
+
+    private boolean isValidFile(Element root){
+        String possibleType = getAttribute(root, "simType");
+        System.out.println(possibleType);
+        return validSimTypes.contains(possibleType);
+    }
+
+    private String getAttribute (Element e, String attributeName) {
+        return e.getAttribute(attributeName);
     }
 
     private Element getRootElement (File xmlFile) {
