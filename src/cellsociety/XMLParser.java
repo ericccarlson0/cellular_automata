8b0e01906c545cf3e1c56731d6e02913194c2d39
Charles Papandreou <cnp20@duke.edu>
2020-02-01 21:34:07 -0500

    Added constructor getdocumentbuilder and getrootelement methods from lab xmlparser file

--- src/cellsociety/XMLParser.java
+++ src/cellsociety/XMLParser.java
@@ -1,4 +1,40 @@
 package cellsociety;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+
 public class XMLParser {
+    private final DocumentBuilder DOCUMENT_BUILDER;
+
+    public XMLParser(){
+        DOCUMENT_BUILDER = getDocumentBuilder();
+    }
+
+    private Element getRootElement (File xmlFile) {
+        try {
+            DOCUMENT_BUILDER.reset();
+            Document xmlDocument = DOCUMENT_BUILDER.parse(xmlFile);
+            return xmlDocument.getDocumentElement();
+        }
+        catch (SAXException | IOException e) {
+            throw new XMLException(e);
+        }
+    }
+
+    private DocumentBuilder getDocumentBuilder () {
+        try {
+            return DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        }
+        catch (ParserConfigurationException e) {
+            throw new XMLException(e);
+        }
+    }
+
 }
