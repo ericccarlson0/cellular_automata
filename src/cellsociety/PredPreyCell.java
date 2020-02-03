255c129d1bbfa8f7e2c14c07ceecf67b5fc5a96c
Turner Jordan <tgj5@duke.edu>
2020-02-03 08:07:31 -0500

    worked on pred/prey simulation

--- src/cellsociety/PredPreyCell.java
+++ src/cellsociety/PredPreyCell.java
@@ -2,19 +2,48 @@ package cellsociety;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 
 public class PredPreyCell extends Cell {
+    public static final Paint SHARK_COLOR = Color.LIGHTSLATEGRAY;
+    public static final Paint FISH_COLOR = Color.LIGHTSEAGREEN;
+    public static final Paint EMPTY_COLOR = Color.SKYBLUE;
 
-    public PredPreyCell(double width, double height, int currState){
-        super(width,height,currState);
+    private double reproductionRate;
+    private double lifeSpan = 0;
+
+    enum PredPreyCellState{
+        SHARK, FISH, EMPTY
+    }
+
+    public PredPreyCell(double width, double height, String currState, double reproductionRate){
+        super(width,height,PredPreyCellState.valueOf(currState));
+        this.reproductionRate = reproductionRate;
+        changeDisplay();
     }
 
     public void calcNewState(ArrayList<HashSet<Cell>> emptySpaces){
+        if(currState == PredPreyCellState.EMPTY && nextState == null) {
+            nextState = PredPreyCellState.EMPTY;
+            emptySpaces.get(1).add(this);
+        }
+        else if(currState == PredPreyCellState.SHARK) {
+            int index = 1;
 
+        }
     }
 
     public void changeDisplay(){
-
+        if(currState == PredPreyCellState.EMPTY) {
+            visual.setFill(EMPTY_COLOR);
+        }
+        else if(currState == PredPreyCellState.SHARK) {
+            visual.setFill(SHARK_COLOR);
+        }
+        else {
+            visual.setFill(FISH_COLOR);
+        }
     }
 }
