0a32e0384c9c4a6a6c806b88aef1e30b44311b92
Eric Carlson <ecc45@duke.edu>
2020-02-06 16:59:31 -0500

    made helper methods for Polygon instances

--- src/cellsociety/GridDisplay.java
+++ src/cellsociety/GridDisplay.java
@@ -4,16 +4,47 @@ import javafx.scene.layout.GridPane;
 
 public class GridDisplay {
     public static final double CELL_GAP = .1;
-
-    private GridPane display;
+    private GridPane gridDisplay;
     private CellShape cellShape;
     private int size;
 
-    enum CellShape{
-        SQUARE
+    enum CellShape {
+        SQUARE, DIAMOND, TRIANGLE, HEXAGON, CIRCLE
     }
 
-    public GridDisplay(String cellShape,Cell[][] initCellConfig){
+    //***
+    private double[] calcPolygonLengths(double radius, int n) {
+        double[] ret = new double[2];
+        ret[0] = 2*radius*Math.sin(Math.PI/n);  // SIDE LENGTH
+        ret[1] = radius*Math.cos(Math.PI/n);    // APOTHEM
+        return ret;
+    }
+
+    private double[] calcTriangleWH(double radius) {
+        double[] lengths = calcPolygonLengths(radius, 3);
+        double[] ret = new double[2];
+        ret[0] = lengths[0];                    // WIDTH
+        ret[1] = lengths[0]*(Math.sqrt(3)/2);   // HEIGHT
+        return ret;
+    }
+
+    private double[] calcHexagonWH(double radius) {
+        double[] lengths = calcPolygonLengths(radius, 6);
+        double[] ret = new double[2];
+        ret[0] = radius*2;                      // WIDTH
+        ret[1] = lengths[1]*2;                  // HEIGHT
+        return ret;
+    }
+
+    private Double[] calcTrianglePoints() {
+        return null;
+    }
+
+    private Double[] calcHexagonPoints() {
+        return null;
+    }
+
+    public GridDisplay(String cellShape, Cell[][] initCellConfig){
         this.cellShape = CellShape.valueOf(cellShape);
         size = initCellConfig.length;
         initializeDisplay(initCellConfig);
@@ -28,18 +59,18 @@ public class GridDisplay {
     }
 
     private void initSquareCellDisplay(Cell[][] config) {
-        display = new GridPane();
-        display.setGridLinesVisible(false);
-        display.setHgap(CELL_GAP);
-        display.setVgap(CELL_GAP);
+        gridDisplay = new GridPane();
+        gridDisplay.setGridLinesVisible(false);
+        gridDisplay.setHgap(CELL_GAP);
+        gridDisplay.setVgap(CELL_GAP);
         for (int row = 0; row < size; row++){
             for (int col = 0; col < size; col++){
-                display.add(config[row][col].getVisual(), col, row,1,1);
+                gridDisplay.add(config[row][col].getVisual(), col, row,1,1);
             }
         }
     }
 
     public GridPane getDisplay(){
-        return display;
+        return gridDisplay;
     }
 }
